06161f985bf6bd547bde7da7332c35665f1114d9
ZenzeroBro <cjb853@nau.edu>
2025-04-19 16:00:54 -0700

    Started working on how the api will be called and how we will store what we get from the api

---
+++ app/src/main/java/com/clw549/blackjackapp/network/RetrofitClient.kt
@@ -0,0 +1,22 @@
+package com.clw549.blackjackapp.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private const val BASE_URL = "https://deckofcardsapi.com/api/"
+
+    // Create the Retrofit instance
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    // Expose the DogApiService instance
+    val apiService: CardApiService by lazy {
+        retrofit.create(CardApiService::class.java)
+    }
+}
---
+++ app/src/main/java/com/clw549/blackjackapp/network/model/CardResponse.kt
@@ -0,0 +1,9 @@
+package com.clw549.blackjackapp.network.model
+
+// Data class to represent the API response
+data class CardResponse (
+    val success: Boolean,   //s API response status (e.g., "true")
+    val deck_id: String, // ID of the deck
+    val cards: List<Card>, // List of drawn cards
+    val remaining: Int // Number of cards remaining in the deck
+)
