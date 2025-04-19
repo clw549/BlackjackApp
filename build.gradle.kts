97493636854998006ee1dd6a1b6c31d549c91989
ZenzeroBro <165959392+ZenzeroBro@users.noreply.github.com>
2025-04-19 16:08:04 -0700

    Revert "Started working on how the api will be called and how we will store w…"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    extra.apply {
-        set("room_version", "2.6.0")
-    }
-}
-
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("com.android.library") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
